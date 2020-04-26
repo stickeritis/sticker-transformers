80d192c45a326ea85882ebe2ae6836d43afb7a37
Daniël de Kok <me@danieldk.eu>
2020-04-26 17:12:22 +0200

    Bump version to 0.6.1

--- nix/Cargo.nix
+++ nix/Cargo.nix
@@ -1999,7 +1999,7 @@ rec {
       };
       "sticker-transformers" = rec {
         crateName = "sticker-transformers";
-        version = "0.6.0";
+        version = "0.6.1";
         edition = "2018";
         src = (builtins.filterSource sourceFilter ../.);
         authors = [
